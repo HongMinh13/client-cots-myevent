6b2292c41e925ef8d39818996a0a721b3e4f7cbe
khanh huyen <ngohuyen.280703@gmail.com>
2024-09-11 21:37:23 +0700

    update client-seenEventList

--- src/pages/Client/event/EventDetail.tsx
+++ src/pages/Client/event/EventDetail.tsx
@@ -1,148 +1,378 @@
-// import { useGetEventQuery } from '#/generated/schemas';
-// import { EVENT_DEFAULT } from '#/shared/utils/constant';
-// import styled from '@emotion/styled';
-// import { Alert, Col, Image, Row, Table, Typography } from 'antd';
-// import { useParams } from 'react-router-dom';
-// import { formatCurrency } from '#/shared/utils/tools';
-// import { CreateEventContract } from '#/pages/Client/event/CreateEventContract';
-// import { userVar } from '#/graphql/cache';
-// import { ROLE } from '#/shared/utils/type';
 
-// const EventDetailPageStyles = styled.div`
-//   .ant-input-affix-wrapper {
-//     background-color: white !important;
-//     padding: 10px 20px !important;
-//     border-radius: 50px !important;
+import styled from '@emotion/styled';
+import {
+    Alert,
+    Col,
+    Descriptions,
+    Image,
+    Row,
+    Skeleton,
+    Table,
+    Timeline,
+    Typography,
+} from 'antd';
+import { useParams } from 'react-router-dom';
+import { formatCurrency } from '#/shared/utils/tools';
+import { ROLE } from '#/shared/utils/type';
+import {
+    useGetEventByIdQuery
+} from '#/generated/schemas';
+import { useState } from 'react';
+import dayjs from 'dayjs';
 
-//     input[value] {
-//       color: black !important;
-//     }
+const EventDetailPageStyles = styled.div`
+  .ant-input-affix-wrapper {
+    background-color: white !important;
+    padding: 10px 20px !important;
+    border-radius: 0.25rem !important;
 
-//     .anticon {
-//       margin-right: 10px !important;
+    input[value] {
+      color: black !important;
+    }
 
-//       svg {
-//         color: black !important;
-//         font-size: 20px !important;
-//       }
-//     }
-//   }
+    .anticon {
+      margin-right: 10px !important;
 
-//   .ant-card-meta-detail {
-//     width: 100%;
+      svg {
+        color: black !important;
+        font-size: 20px !important;
+      }
+    }
+  }
 
-//     .ant-card-meta-title {
-//       font-size: 16px;
-//       font-weight: bold;
-//     }
+  .ant-card-meta-detail {
+    width: 100%;
 
-//     .ant-card-meta-description .day {
-//       color: orange;
-//     }
-//   }
-// `;
+    .ant-card-meta-title {
+      font-size: 16px;
+      font-weight: bold;
+    }
 
-// export function EventDetailPage() {
-//   const { id } = useParams();
-//   const { data: event, loading } = useGetEventQuery({
-//     variables: {
-//       input: id as any,
-//     },
-//   });
-//   const user = userVar();
+    .ant-card-meta-description .day {
+      color: orange;
+    }
+  }
+`;
 
-//   const columns = [
-//     {
-//       title: 'Tên dịch vụ',
-//       dataIndex: ['serviceItem', 'name'],
-//       key: 'name',
-//     },
-//     {
-//       title: 'Mô tả',
-//       dataIndex: ['serviceItem', 'description'],
-//       key: 'description',
-//       render: (text: string) => (
-//         <div dangerouslySetInnerHTML={{ __html: text || '' }} />
-//       ),
-//     },
-//     {
-//       title: 'Giá',
-//       dataIndex: ['serviceItem', 'price'],
-//       key: 'price',
-//       render: (price: number) => <div>{formatCurrency(price)}</div>,
-//     },
-//     {
-//       title: 'Số lượng',
-//       dataIndex: 'amount',
-//       key: 'amount',
-//     },
-//   ];
+export interface ServiceColumn {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    quantity: number;
+    img?: string | null;
+}
 
-//   return (
-//     <EventDetailPageStyles className="content ">
-//       <Row className="mx-auto min-h-[1000px]  w-full max-w-container py-8 ">
-//         <Typography.Title level={1} className="w-full text-center text-red-500">
-//           {event?.getEvent.name}
-//         </Typography.Title>
-//         <text className="pb-8 text-xl text-black">
-//           Polaris Event – với hơn 10 năm xây dựng và phát triển với những thành
-//           tựu nhất định trong việc tổ chức nhiều sự kiện thành công cho các cơ
-//           quan, đơn vị và doanh nghiệp trên toàn quốc. Polaris Event luôn tự tin
-//           là một trong những công ty tổ chức sự kiện hàng đầu tại Việt Nam. Đơn
-//           vị chuyên hỗ trợ, tư vấn, cung cấp dịch vụ {event?.getEvent.name} trọn
-//           gói từ Bắc – Nam với mức giá cạnh tranh, hợp lý nhất.
-//         </text>
-//         <Image
-//           src={`${event?.getEvent.thumbnail ?? EVENT_DEFAULT}`}
-//           preview={false}
-//         />
-//         <div
-//           dangerouslySetInnerHTML={{ __html: event?.getEvent.detail || '' }}
-//           className="py-8 text-black"
-//         />
-//         {Number(event?.getEvent?.eventServiceItems?.length) > 0 && (
-//           <Row
-//             className="my-4 w-full rounded-lg bg-white p-2"
-//             gutter={[16, 16]}
-//           >
-//             <Col span={24}>
-//               <Typography.Title level={3} className="text-[#fefeff]">
-//                 Các dịch vụ kèm theo
-//               </Typography.Title>
-//             </Col>
-//             <Col span={24}>
-//               <Table
-//                 columns={columns}
-//                 dataSource={event?.getEvent?.eventServiceItems ?? []}
-//                 loading={loading}
-//                 rowKey="id"
-//                 pagination={false}
-//               />
-//             </Col>
-//             {event?.getEvent?.eventServiceItems?.length &&
-//               localStorage.getItem("role") === ROLE.USER && (
-//                 <Col span={24} className={'mt-4'}>
-//                   <Alert
-//                     className={'rounded-md py-4 shadow-lg'}
-//                     message={
-//                       <Row className={'flex items-center justify-between'}>
-//                         <Typography.Text className={'text-lg text-black'}>
-//                           Bạn có nhu cầu tổ chức sự kiện này. Nhấn vào nút bên
-//                           cạnh để đăng kí ngay nhé!
-//                         </Typography.Text>
-//                         <Col span={6}>
-//                           <CreateEventContract event={event?.getEvent as any} />
-//                         </Col>
-//                       </Row>
-//                     }
-//                     type="info"
-//                     showIcon
-//                   />
-//                 </Col>
-//               )}
-//           </Row>
-//         )}
-//       </Row>
-//     </EventDetailPageStyles>
-//   );
-// }
+export function EventDetailPage() {
+    const { id } = useParams();
+    const [serviceData, setServiceData] = useState<ServiceColumn[]>([]);
+    const { data: event, loading } = useGetEventByIdQuery({
+        variables: {
+            getEventByIdId: String(id),
+        },
+        fetchPolicy: 'no-cache',
+        onCompleted: data => {
+            const devices =
+                data.getEventById.rental?.devices?.map((device, index) => ({
+                    id: device.id,
+                    name: device.name,
+                    description: device.description,
+                    price: device.hourlyRentalFee,
+                    quantity: device.quantity,
+                    img: device.img,
+                })) ?? [];
+
+            const humanResources =
+                data?.getEventById?.rental?.humanResources?.map(
+                    (humanResource, index) => ({
+                        id: humanResource.id,
+                        name: humanResource.name,
+                        description: humanResource.description,
+                        price: humanResource.hourlySalary,
+                        quantity: humanResource.quantity,
+                        img: '',
+                    }),
+                ) ?? [];
+
+            setServiceData([...devices, ...humanResources]);
+            // console.log('data', data);
+        },
+    });
+    console.log('event', event);
+
+    const deviceColumn = [
+        {
+            title: 'Tên thiết bị',
+            dataIndex: 'name',
+            key: 'name',
+            render: (text: any, record: any) => (
+                <Typography.Text className="font-bold text-black">
+                    {record.name}
+                </Typography.Text>
+            ),
+        },
+        {
+            title: 'Hình ảnh',
+            dataIndex: 'img',
+            key: 'image',
+            render: (img: string) => (
+                <Image src={img} alt="image" width={50} height={50} />
+            ),
+        },
+        {
+            title: 'Mô tả',
+            dataIndex: 'description',
+            key: 'description',
+            render: (text: string) => (
+                <div dangerouslySetInnerHTML={{ __html: text || '' }} />
+            ),
+        },
+        {
+            title: 'Số lượng',
+            dataIndex: 'quantity',
+            key: 'quantity',
+            render: (quantity: number) => (
+                <Typography.Text className="text-black">{quantity}</Typography.Text>
+            ),
+        },
+        {
+            title: 'Đơn giá / ngày',
+            dataIndex: 'hourlyRentalFee',
+            key: 'hourlyRentalFee',
+            render: (hourlyRentalFee: number) => (
+                <Typography.Text className="text-black">
+                    {formatCurrency(hourlyRentalFee)}
+                </Typography.Text>
+            ),
+        },
+    ];
+
+    const humanResourceColumn = [
+        {
+            title: 'Tên loại hình',
+            dataIndex: 'name',
+            key: 'name',
+            render: (text: any, record: any) => (
+                <Typography.Text className="font-bold text-black">
+                    {record.name}
+                </Typography.Text>
+            ),
+        },
+        {
+            title: 'Hình ảnh',
+            dataIndex: 'img',
+            key: 'image',
+            render: (img: string) => (
+                <Image src={img} alt="image" width={50} height={50} />
+            ),
+        },
+        {
+            title: 'Mô tả',
+            dataIndex: 'description',
+            key: 'description',
+            render: (text: string) => (
+                <div dangerouslySetInnerHTML={{ __html: text || '' }} />
+            ),
+        },
+        {
+            title: 'Số lượng',
+            dataIndex: 'quantity',
+            key: 'quantity',
+            render: (quantity: number) => (
+                <Typography.Text className="text-black">{quantity}</Typography.Text>
+            ),
+        },
+        {
+            title: 'Đơn giá / ngày',
+            dataIndex: 'hourlySalary',
+            key: 'hourlySalary',
+            render: (hourlyRentalFee: number) => (
+                <Typography.Text className="text-black">
+                    {formatCurrency(hourlyRentalFee)}
+                </Typography.Text>
+            ),
+        },
+    ];
+
+    const locationColumn = [
+        {
+            title: 'Tên địa điểm',
+            dataIndex: 'name',
+            key: 'name',
+            render: (text: any, record: any) => (
+                <Typography.Text className="font-bold text-black">
+                    {record.name}
+                </Typography.Text>
+            ),
+        },
+        {
+            title: 'Địa chỉ',
+            dataIndex: 'address',
+            key: 'address',
+            render: (text: any, record: any) => (
+                <Typography.Text className="text-black">
+                    {record.address}
+                </Typography.Text>
+            ),
+        },
+        {
+            title: 'Hình ảnh',
+            dataIndex: 'img',
+            key: 'image',
+            render: (img: string) => (
+                <Image src={img} alt="image" width={50} height={50} />
+            ),
+        },
+        {
+            title: 'Mô tả',
+            dataIndex: 'description',
+            key: 'description',
+            render: (text: string) => (
+                <div dangerouslySetInnerHTML={{ __html: text || '' }} />
+            ),
+        },
+
+        {
+            title: 'Đơn giá / ngày',
+            dataIndex: 'hourlyRentalFee',
+            key: 'hourlyRentalFee',
+            render: (hourlyRentalFee: number) => (
+                <Typography.Text className="text-black">
+                    {formatCurrency(hourlyRentalFee)}
+                </Typography.Text>
+            ),
+        },
+    ];
+    return (
+        <Skeleton loading={loading} active>
+            <EventDetailPageStyles className="content ">
+                <Row className="mx-auto min-h-[1000px]  w-full max-w-container py-8 ">
+                    <Typography.Title
+                        level={1}
+                        className="text-[#0f172a]-500 w-full text-center"
+                    >
+                        {event?.getEventById.name}
+                    </Typography.Title>
+                    <text className="pb-8 text-xl text-black">
+                        {event?.getEventById?.description || ''}
+                    </text>
+                    <Image src={`${event?.getEventById.img}`} preview={false} />
+                    {/* <div
+            dangerouslySetInnerHTML={{
+              __html: event?.getEventById?.description || '',
+            }}
+            className="py-8 text-black"
+          /> */}
+                    <div
+                        dangerouslySetInnerHTML={{
+                            __html: event?.getEventById?.detail || '',
+                        }}
+                        className="py-8 text-black"
+                    />
+
+                    {event?.getEventById.rental?.timelines?.length && (
+                        <Row
+                            className="my-4 w-full rounded-lg bg-white p-2"
+                            gutter={[16, 16]}
+                        >
+                            <Col span={24} className="mt-16">
+                                <Typography.Title level={3} className="text-black">
+                                    Lịch trình dự kiến của sự kiện:
+                                </Typography.Title>
+                            </Col>
+
+                            <Col span={24}>
+                                <Descriptions layout="vertical" bordered>
+                                    {event?.getEventById?.rental?.devices?.length && (
+                                        <Descriptions.Item span={3} label="Lịch trình">
+                                            <Timeline mode={'left'} className='text-black'>
+                                                {event?.getEventById.rental?.timelines?.map(
+                                                    (item, index) => {
+                                                        return (
+                                                            <Timeline.Item
+                                                                label={dayjs(item.startTime).format(
+                                                                    'HH:mm',
+                                                                )}
+                                                            >
+                                                                {item?.description}
+                                                            </Timeline.Item>
+                                                        );
+                                                    },
+                                                )}
+                                            </Timeline>
+                                        </Descriptions.Item>
+                                    )}
+                                </Descriptions>
+                            </Col>
+                        </Row>
+                    )}
+
+                    {serviceData.length && (
+                        <Row
+                            className="my-4 w-full rounded-lg bg-white p-2"
+                            gutter={[16, 16]}
+                        >
+                            <Col span={24} className="mt-16">
+                                <Typography.Title level={3} className="text-black">
+                                    Các dịch vụ cung cấp:
+                                </Typography.Title>
+                            </Col>
+
+                            <Col span={24}>
+                                <Descriptions layout="vertical" bordered>
+                                    {event?.getEventById?.rental?.devices?.length && (
+                                        <Descriptions.Item span={3} label="Thiết bị">
+                                            <Table
+                                                columns={deviceColumn}
+                                                dataSource={event?.getEventById?.rental?.devices}
+                                                pagination={false}
+                                            />
+                                        </Descriptions.Item>
+                                    )}
+
+                                    {event?.getEventById?.rental?.humanResources?.length && (
+                                        <Descriptions.Item span={3} label="Nhân lực">
+                                            <Table
+                                                columns={humanResourceColumn}
+                                                dataSource={event?.getEventById?.rental?.humanResources}
+                                                pagination={false}
+                                            />
+                                        </Descriptions.Item>
+                                    )}
+
+                                    {event?.getEventById?.rental?.locations?.length && (
+                                        <Descriptions.Item span={3} label="Địa điểm">
+                                            <Table
+                                                columns={locationColumn}
+                                                dataSource={event?.getEventById?.rental?.locations}
+                                                pagination={false}
+                                            />
+                                        </Descriptions.Item>
+                                    )}
+                                </Descriptions>
+                            </Col>
+                        </Row>
+                    )}
+                    {localStorage.getItem('role') === ROLE.USER && (
+                        <Col span={24} className={'mt-4'}>
+                            <Alert
+                                className={'rounded-md py-4 shadow-lg'}
+                                message={
+                                    <Row className={'flex items-center justify-between'}>
+                                        <Typography.Text className={'text-lg text-black'}>
+                                            Bạn có nhu cầu tổ chức sự kiện này. Nhấn vào nút bên cạnh
+                                            để đăng kí ngay nhé!
+                                        </Typography.Text>
+
+                                    </Row>
+                                }
+                                type="info"
+                                showIcon
+                            />
+                        </Col>
+                    )}
+                </Row>
+            </EventDetailPageStyles>
+        </Skeleton>
+    );
+}
